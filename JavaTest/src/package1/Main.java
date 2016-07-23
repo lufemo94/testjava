497509d4012c95670e6d5b1215de98c2376c3a0f
lucas <lucas@192.168.0.105>
2016-07-23 20:36:01 -0300

    Created the method sum

--- JavaTest/src/package1/Main.java
+++ JavaTest/src/package1/Main.java
@@ -3,6 +3,8 @@ package package1;
 public class Main {
 	private static void printMessage(){
 		System.out.println("Hello world");
+		System.out.println("How are you?");
+		System.out.println("2 + 2 = " + sum(2, 2));
 	}
 
 	public static void main(String[] args) {
@@ -13,5 +15,7 @@ public class Main {
 		System.out.println(horse);
 	}
 	
-	
+	public static int sum(int a, int b){
+		return a+b;
+	}
 }

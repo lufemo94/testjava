f9e005dc0b919a4b4c7308f3fe7e7e39ffc20d98
lucas <lucas@192.168.0.105>
2016-07-23 20:32:25 -0300

    Created the method to print a message

--- JavaTest/src/package1/Main.java
+++ JavaTest/src/package1/Main.java
@@ -1,9 +1,12 @@
 package package1;
 
 public class Main {
+	private static void printMessage(){
+		System.out.println("Hello world");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		printMessage();
 	}
 
 }
